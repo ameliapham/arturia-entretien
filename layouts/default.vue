90b70ef4a95dfc04e5a437e7f936fea7ac8d0341
ameliapham <ptt.huong.8895@gmail.com>
2025-02-07 03:36:36 +0100

    Update default

---
+++ layouts/default.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from '~/components/Header.vue'
+import Footer from '~/components/Footer.vue'
+</script>
+
+<template>
+  <div class="layout">
+    <Header ></Header>
+    <main>
+      <slot ></slot>
+    </main>
+    <Footer ></Footer>
+  </div>
+</template>
+
+
+<style scoped lang="scss">
+.layout {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+
+  main {
+    flex: 1;
+  }
+}
+</style>
