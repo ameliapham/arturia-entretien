1768d04377864dea87b20267bc0b17fc78168211
ameliapham <ptt.huong.8895@gmail.com>
2025-02-07 03:37:57 +0100

    Create Link

---
+++ components/Link.vue
@@ -0,0 +1,22 @@
+<template>
+    <a :href="href" class="link">
+        <slot ></slot>
+    </a>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+    href: string
+}>()
+</script>
+
+<style scoped lang="scss">
+.link {
+    color: blue;
+    text-decoration: none;
+
+    &:hover {
+        text-decoration: underline;
+    }
+}
+</style>
