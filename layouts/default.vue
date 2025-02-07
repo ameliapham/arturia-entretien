64f3231391b57c367dfb4d766ca4e981bdc38d6b
Huong Pham <59421719+ameliapham@users.noreply.github.com>
2025-02-07 11:23:12 +0000

    Import bootstrap as a global stylesheet

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from '~/components/Header.vue'
-import Footer from '~/components/Footer.vue'
+import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
 </script>
 
 <template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   css: [
+    '@/assets/css/bootstrap-custom.css',
     '@/assets/scss/_main.scss'
   ],
   
